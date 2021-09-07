12fac5c80d9d8dd8b265d619b218886d35641de0
amartya <amartyasinha918@gmail.com>
2021-09-07 13:01:56 +0530

    Python Lab

---
+++ TrianglePattern.py
@@ -0,0 +1,24 @@
+n = int(input("Enter n: "))
+
+choice = int(input("Select from the given patterns:\n1. Right Triangle\n2. Inverted Triangle\n\nChoice: "))
+
+if choice == 1:
+    for i in range(1, n + 1):
+        for j in range(1, i + 1):
+            print("* ", end="")
+        print()
+
+elif choice == 2:
+    for i in range(n, 0, -1):
+        for space in range(0, n - i):
+            print("  ", end="")
+
+        for j in range(i, 2 * i):
+            print("* ", end="")
+
+        for j in range(1, i):
+            print("* ", end="")
+        print()
+
+else:
+    print("Wrong Choice!\n")
